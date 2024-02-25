6d5b7ab281878ea69927c190a7b1efc2fb85dd43
abidshaikh <abidshaikh1941@gmail.com>
2024-02-25 15:57:30 +0530

    Adjacency List Graph BFS & DFS

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import graph.DirectedAdjacencyGraph;
+import graph.UnDirectedAdjacencyGraph;
+
 public class Main {
     public static void main(String[] args) {
+        graphDriverCode();
+    }
+
+    private static void graphDriverCode() {
         System.out.println("Hello world!");
+        UnDirectedAdjacencyGraph directedAdjacentGraph = new UnDirectedAdjacencyGraph();
+        directedAdjacentGraph.addEdge(0, 1);
+        directedAdjacentGraph.addEdge(0, 2);
+        directedAdjacentGraph.addEdge(1, 2);
+        directedAdjacentGraph.addEdge(2, 0);
+        directedAdjacentGraph.addEdge(2, 3);
+        directedAdjacentGraph.addEdge(3, 3);
+
+        directedAdjacentGraph.printWholeGraph();
+        directedAdjacentGraph.dfs("0");
+        directedAdjacentGraph.bfs("2");
     }
 }
